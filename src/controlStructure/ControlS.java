dc0060da1c2e936108b492b266840f0f5fe6310e
user1 <user1@user>
2017-08-22 14:34:56 +0100

    Control Structure

---
+++ src/controlStructure/ControlS.java
@@ -0,0 +1,20 @@
+package controlStructure;
+
+public class ControlS {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a =5;
+		int b = 10;
+		int c= 20;
+		
+		if(a<b & b<c ) {        
+			System.out.println(c);
+		}else {
+			System.out.println(a + " " + " " + b  );
+		}
+		
+	}
+
+}
